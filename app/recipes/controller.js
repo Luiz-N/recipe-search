27e3ab4f605c22e2b370cd1f7466514274333c77
Luiz <luiz.neves7@gmail.com>
2016-01-03 00:33:17 -0600

    add fancy loading action

--- app/recipes/controller.js
+++ app/recipes/controller.js
@@ -6,13 +6,18 @@ export default Ember.Controller.extend({
 
   noSelectedRecipe: Ember.computed('target.currentPath', function() {
     // see if current path is heading for a specific recipe
-    // if not then show greeting box
+    // if not then show greeting box. feels hackish... probably a better way
     return this.target.currentPath != "recipes.recipe";
   }),
 
   actions: {
     search: function() {
       this.send('refreshModel');
+    },
+
+    setQuery: function(query) {
+      this.set('q', query);
+      this.send('search');
     }
   }
 })
--- app/recipes/route.js
+++ app/recipes/route.js
@@ -1,13 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(params) {
+  model(params) {
     return this.get('store').query('recipe', {q: params.q})
   },
 
   actions: {
-    refreshModel: function() {
+    refreshModel() {
       this.refresh();
+    },
+    loading(transition) {
+      let controller = this.controllerFor('recipes');
+      controller.set('currentlyLoading', true);
+      transition.promise.finally(function() {
+          controller.set('currentlyLoading', false);
+      });
     }
   }
 });
